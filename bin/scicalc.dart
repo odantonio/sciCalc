bd5d00962e18ecf4853dffe37ab992b305a97b00
odantonio <obdantonio@terra.com.br>
2023-09-10 19:24:26 -0300

    inicializando projeto

---
+++ bin/scicalc.dart
@@ -0,0 +1,5 @@
+import 'package:scicalc/scicalc.dart' as scicalc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${scicalc.calculate()}!');
+}
---
+++ test/scicalc_test.dart
@@ -0,0 +1,8 @@
+import 'package:scicalc/scicalc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
